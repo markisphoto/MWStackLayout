ddfcd51701cf618b8f1e4f348574bfbac700e036
Mark <warnick.mark@me.com>
2013-06-06 21:18:53 -0500

    Added custom UICollectionViewFlowLayout subclass which handles laying out our collectionView

---
+++ MWStackLayout/MWStackLayout.h
@@ -0,0 +1,13 @@
+//
+//  MWStackLayout.h
+//  MWStackLayout
+//
+//  Created by Mark Warnick on 6/6/13.
+//  Copyright (c) 2013 Mark Warnick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWStackLayout : UICollectionViewFlowLayout
+
+@end
---
+++ MWStackLayout/MWStackLayout.m
@@ -0,0 +1,13 @@
+//
+//  MWStackLayout.m
+//  MWStackLayout
+//
+//  Created by Mark Warnick on 6/6/13.
+//  Copyright (c) 2013 Mark Warnick. All rights reserved.
+//
+
+#import "MWStackLayout.h"
+
+@implementation MWStackLayout
+
+@end
